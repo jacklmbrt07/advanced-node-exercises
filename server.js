60da3cf2a6bb63726bbfd22b1f1fa87126fa841f
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-12 11:04:52 -0800

    2/22

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('/Users/jacklambertwork/Desktop/free-code-camp/mochachai-testing/advanced-node-exercises/views/pug');
+app.route("/").get((req, res) => {
+  res.render(process.cwd() + "/views/pug/index", {
+    title: "Hello",
+    message: "Please login",
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Listening on port ' + process.env.PORT);
+  console.log("Listening on port " + process.env.PORT);
 });
