cd49af87b7c0d48b1aed4745b47481642e0eb1ec
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-12 11:42:52 -0800

    5/22

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session");
 const passport = require("passport");
 const ObjectID = require("mongodb").ObjectID;
+const LocalStrategy = require("passport-local");
 
 const app = express();
 app.set("view engine", "pug");
@@ -48,12 +49,24 @@ myDB(async (client) => {
       done(null, doc);
     });
   });
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
+      myDataBase.findOne({ username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (password !== user.password) return done(null, false);
+        return done(null, user);
+      });
+    })
+  );
   // Be sure to add this...
 }).catch((e) => {
   app.route("/").get((req, res) => {
     res.render("pug", { title: e, message: "Unable to login" });
   });
 });
+
 // app.listen out here...
 
 app.listen(process.env.PORT || 3000, () => {
