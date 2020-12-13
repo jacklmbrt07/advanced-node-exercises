22ab4c798378ec08f832cd22397a2b8f2885857a
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-13 07:45:21 -0800

    13/22

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const bcrypt = require('bcrypt');
-const ObjectID = require('mongodb').ObjectID;
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
+const ObjectID = require("mongodb").ObjectID;
+const GitHubStrategy = require("passport-github").Strategy;
+require("dotenv").config();
 
 module.exports = function (app, myDataBase) {
   passport.serializeUser((user, done) => {
@@ -13,15 +15,35 @@ module.exports = function (app, myDataBase) {
       done(null, doc);
     });
   });
-  passport.use(new LocalStrategy(
-    function(username, password, done) {
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
       myDataBase.findOne({ username: username }, function (err, user) {
-        console.log('User '+ username +' attempted to log in.');
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (password !== user.password) { return done(null, false); }
+        console.log("User " + username + " attempted to log in.");
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (password !== user.password) {
+          return done(null, false);
+        }
         return done(null, user);
       });
-    }
-  ));
+    })
+  );
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL:
+          "https://advanced-node-exercises.herokuapp.com/auth/github/callback",
+      },
+      function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        // Database logic goes here with cb containing user object
+      }
+    )
+  );
 };
